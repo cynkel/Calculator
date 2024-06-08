3fa3d7eeeacfe78aa2c33f5c3888729eafc4584b
Kelvin <kelxir@outlook.com>
2024-06-08 16:28:56 +0000

    Add UI to make numbers smaller when more numbers are added
    Implemented a function that gets called when characters reach a certain
    limit and decreases font size to allow for more characters.

--- script.js
+++ script.js
@@ -35,4 +35,32 @@ let firstNumber = 0;
 let secondNumber = 1;
 let operator = "*";
 
-console.log(operate(firstNumber, secondNumber, operator));
+const display = document.querySelector(".live");
+const numberButton = document.querySelectorAll(".number");
+const test = numberButton[0].textContent;
+
+function populateDisplay(numberToPopulate) {
+    display.textContent += numberToPopulate;
+    setTextLength();
+}
+
+function setTextLength() {
+    switch(display.textContent.length) {
+        case 9:
+            display.style.fontSize = "40px";
+            break;
+        case 10:
+            display.style.fontSize = "36px";
+            break;
+        case 12:
+            display.style.fontSize = "24px";
+            break;
+        case 18:
+            display.style.fontSize = "18px";
+            break;
+    }
+}
+
+for (let index = 0; index < numberButton.length; index++) {
+    numberButton[index].addEventListener("click", () => populateDisplay(numberButton[index].textContent));  
+}
